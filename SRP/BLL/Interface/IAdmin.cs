5f7a7431d897a7a041fc4fe873944e93c1f57b28
BerkBasat <berk.basat@bilgeadamakademi.com>
2022-04-22 19:32:22 +0300

    BLL created

---
+++ SRP/BLL/Interface/IAdmin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Model.Entity;
+
+namespace BLL.Interface
+{
+    public interface IAdmin<T> where T : BaseEntity
+    {
+        string Create(T model);
+
+        string Update(T model);
+
+        string Delete(int id);
+
+        List<T> GetList();
+    }
+}
---
+++ SRP/BLL/Interface/IUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Model.Entity;
+
+namespace BLL.Interface
+{
+    public interface IUser<T> where T : BaseEntity
+    {
+        string Comment(Comment comment);
+
+        List<T> GetList();
+    }
+}
