855c0c5d20ab2170388572daeb7d944d5405c78b
BerkBasat <berk.basat@bilgeadamakademi.com>
2022-04-24 13:12:09 +0300

    Base class created

---
+++ LiskovSubstitution/LiskovSubstitution/BaseSubscriber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitution
+{
+    public class BaseSubscriber
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        public virtual void GiveAccessToFamilyMembers()
+        {
+            Console.WriteLine("Access granted to family members");
+        }
+
+        public virtual void AccessToLimitedTitles()
+        {
+            Console.WriteLine("Access to limited titles granted");
+        }
+
+        public virtual void AccessToUnlimitedContent()
+        {
+            Console.WriteLine("Access to unlimited content");
+        }
+    }
+}
