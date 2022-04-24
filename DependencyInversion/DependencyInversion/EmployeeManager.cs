44ce04d01bb4657373847d4a4110c400b4c15ac5
BerkBasat <berk.basat@bilgeadamakademi.com>
2022-04-24 18:28:30 +0300

    DIP completed

---
+++ DependencyInversion/DependencyInversion/EmployeeManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversion
+{
+    public class EmployeeManager : IEmployeeSearch
+    {
+        private List<Employee> _employees;
+
+        public EmployeeManager()
+        {
+            _employees = new List<Employee>();
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            _employees.Add(employee);
+        }
+
+        public IEnumerable<Employee> GetEmployeesByPosition(Position position)
+        {
+            return _employees.Where(x => x.Position == position);
+        }
+    }
+}
---
+++ DependencyInversion/DependencyInversion/IEmployeeSearch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversion
+{
+    public interface IEmployeeSearch
+    {
+        //IEnumerable bir koleksiyon içerisinde dönerek bellek üzerinde muhafaza edilen veriler üzerinden gerekli sorgulama işlemini yapar.
+        IEnumerable<Employee> GetEmployeesByPosition(Position position);
+    }
+}
